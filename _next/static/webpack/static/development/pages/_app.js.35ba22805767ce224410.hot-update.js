ee18f5ca6a16d463d98819ea19f54f5db4efcbdf
John Tomanelli II <jptomanelli@gmail.com>
2020-02-16 18:27:22 -0500

    Build

---
+++ _next/static/webpack/static/development/pages/_app.js.35ba22805767ce224410.hot-update.js
@@ -0,0 +1,18 @@
+webpackHotUpdate("static/development/pages/_app.js",{
+
+/***/ "./node_modules/css-loader/dist/cjs.js?!./node_modules/postcss-loader/src/index.js?!./styles.css":
+/*!********************************************************************************************************************************!*\
+  !*** ./node_modules/css-loader/dist/cjs.js??ref--5-oneOf-4-1!./node_modules/postcss-loader/src??__nextjs_postcss!./styles.css ***!
+  \********************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(/*! ./node_modules/css-loader/dist/runtime/api.js */ "./node_modules/css-loader/dist/runtime/api.js")(true);
+// Module
+exports.push([module.i, "body {\n  font-family: 'SF Pro Text', 'SF Pro Icons', 'Helvetica Neue', 'Helvetica',\n    'Arial', sans-serif;\n  padding: 20px 20px 60px;\n  max-width: 680px;\n  margin: 0 auto;\n  background: #FAEBD7;\n}\n\na {\n  color: #000;\n}\n", "",{"version":3,"sources":["styles.css"],"names":[],"mappings":"AAAA;EACE;uBACqB;EACrB,uBAAuB;EACvB,gBAAgB;EAChB,cAAc;EACd,mBAAmB;AACrB;;AAEA;EACE,WAAW;AACb","file":"styles.css","sourcesContent":["body {\n  font-family: 'SF Pro Text', 'SF Pro Icons', 'Helvetica Neue', 'Helvetica',\n    'Arial', sans-serif;\n  padding: 20px 20px 60px;\n  max-width: 680px;\n  margin: 0 auto;\n  background: #FAEBD7;\n}\n\na {\n  color: #000;\n}\n"]}]);
+
+
+/***/ })
+
+})
+//# sourceMappingURL=_app.js.35ba22805767ce224410.hot-update.js.map
