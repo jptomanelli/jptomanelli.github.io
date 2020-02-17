50b69e42053301dc3885ec66b5116426c8d82957
John Tomanelli II <jptomanelli@gmail.com>
2020-02-16 19:54:19 -0500

    back to black

--- out/_next/static/aLHdFf0lMSH6zr_kxIVgZ/pages/about.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{Juyh:function(n,t,u){"use strict";u.r(t);var e=u("q1tI"),o=u.n(e),c=o.a.createElement;t.default=function(){return Object(e.useEffect)((function(){location.href="/"}),[]),c(o.a.Fragment,null)}},rB5V:function(n,t,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about",function(){return u("Juyh")}])}},[["rB5V",1,2,0]]]);
---
+++ out/_next/static/webpack/static/development/pages/_app.js.21afe72fbb724fad421d.hot-update.js
@@ -0,0 +1,18 @@
+webpackHotUpdate("static/development/pages/_app.js",{
+
+/***/ "./node_modules/css-loader/dist/cjs.js?!./node_modules/postcss-loader/src/index.js?!./styles.css":
+/*!********************************************************************************************************************************!*\
+  !*** ./node_modules/css-loader/dist/cjs.js??ref--5-oneOf-4-1!./node_modules/postcss-loader/src??__nextjs_postcss!./styles.css ***!
+  \********************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(/*! ./node_modules/css-loader/dist/runtime/api.js */ "./node_modules/css-loader/dist/runtime/api.js")(true);
+// Module
+exports.push([module.i, "body {\n  font-family: 'SF Pro Text', 'SF Pro Icons', 'Helvetica Neue', 'Helvetica',\n    'Arial', sans-serif;\n  padding: 20px 20px 60px;\n  max-width: 680px;\n  margin: 0 auto;\n  background: #000;\n}\n\nh1, h2, a {\n  color: #999999;\n}\n\na, img, div, h1, h2 {\n  transition: opacity 250ms ease;\n}\n\n.hidden {\n  opacity: 0;\n}", "",{"version":3,"sources":["styles.css"],"names":[],"mappings":"AAAA;EACE;uBACqB;EACrB,uBAAuB;EACvB,gBAAgB;EAChB,cAAc;EACd,gBAAgB;AAClB;;AAEA;EACE,cAAc;AAChB;;AAEA;EACE,8BAA8B;AAChC;;AAEA;EACE,UAAU;AACZ","file":"styles.css","sourcesContent":["body {\n  font-family: 'SF Pro Text', 'SF Pro Icons', 'Helvetica Neue', 'Helvetica',\n    'Arial', sans-serif;\n  padding: 20px 20px 60px;\n  max-width: 680px;\n  margin: 0 auto;\n  background: #000;\n}\n\nh1, h2, a {\n  color: #999999;\n}\n\na, img, div, h1, h2 {\n  transition: opacity 250ms ease;\n}\n\n.hidden {\n  opacity: 0;\n}"]}]);
+
+
+/***/ })
+
+})
+//# sourceMappingURL=_app.js.21afe72fbb724fad421d.hot-update.js.map
