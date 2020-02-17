5980b2d8d3dcc831ae334f26e03f259eb2a32a83
John Tomanelli II <jptomanelli@gmail.com>
2020-02-16 19:30:17 -0500

    Deploy

---
+++ _next/static/webpack/static/development/pages/index.js.081f0a0ee6d5dccff040.hot-update.js
@@ -0,0 +1,139 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _index_module_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./index.module.css */ "./pages/index.module.css");
+/* harmony import */ var _index_module_css__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_index_module_css__WEBPACK_IMPORTED_MODULE_1__);
+var _jsxFileName = "/Users/obsidian/Desktop/jptomanelli.github.io/pages/index.js";
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+
+
+function HomePage() {
+  var _useState = Object(react__WEBPACK_IMPORTED_MODULE_0__["useState"])(false),
+      loaded = _useState[0],
+      setLoaded = _useState[1];
+
+  Object(react__WEBPACK_IMPORTED_MODULE_0__["useEffect"])(function () {
+    setLoaded(true);
+  }, []);
+  return __jsx("div", {
+    className: "".concat(_index_module_css__WEBPACK_IMPORTED_MODULE_1___default.a.small_container, " ").concat(loaded ? 'active' : ''),
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 13
+    },
+    __self: this
+  }, __jsx("h1", {
+    className: _index_module_css__WEBPACK_IMPORTED_MODULE_1___default.a.title,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 14
+    },
+    __self: this
+  }, "John Tomanelli II"), __jsx("h2", {
+    className: _index_module_css__WEBPACK_IMPORTED_MODULE_1___default.a.sub,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 15
+    },
+    __self: this
+  }, "Full-Stack Web Developer"), __jsx(Link, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 16
+    },
+    __self: this
+  }, __jsx("a", {
+    href: "/about",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 16
+    },
+    __self: this
+  }, "about")), __jsx("div", {
+    className: _index_module_css__WEBPACK_IMPORTED_MODULE_1___default.a.img_container,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 18
+    },
+    __self: this
+  }, __jsx("img", {
+    className: _index_module_css__WEBPACK_IMPORTED_MODULE_1___default.a.profile,
+    src: "profile.svg",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 19
+    },
+    __self: this
+  })), __jsx("div", {
+    className: _index_module_css__WEBPACK_IMPORTED_MODULE_1___default.a.links,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 21
+    },
+    __self: this
+  }, __jsx("a", {
+    target: "_blank",
+    rel: "noopener noreferrer",
+    href: "mailto:jptomanelli@gmail.com;subject",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 22
+    },
+    __self: this
+  }, "email"), __jsx("a", {
+    target: "_blank",
+    rel: "noopener noreferrer",
+    href: "https://github.com/jptomanelli",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 29
+    },
+    __self: this
+  }, "github"), __jsx("a", {
+    target: "_blank",
+    rel: "noopener noreferrer",
+    href: "https://www.linkedin.com/in/johntomanelli/",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 36
+    },
+    __self: this
+  }, "linkedin"), __jsx("a", {
+    target: "_blank",
+    rel: "noopener noreferrer",
+    href: "https://twitter.com/john_tomanelli",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 43
+    },
+    __self: this
+  }, "twitter"), __jsx("a", {
+    target: "_blank",
+    rel: "noopener noreferrer",
+    href: "https://drive.google.com/file/d/1GYmUBbBQIN5veHrlYhmQGVAyugyeTNBE/view?usp=sharing",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 50
+    },
+    __self: this
+  }, "r\xE9sum\xE9")));
+}
+
+/* harmony default export */ __webpack_exports__["default"] = (HomePage);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.081f0a0ee6d5dccff040.hot-update.js.map
---
+++ _next/static/webpack/static/development/pages/index.js.828ac661448814bc678f.hot-update.js
@@ -0,0 +1,27 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./node_modules/css-loader/dist/cjs.js?!./node_modules/postcss-loader/src/index.js?!./pages/index.module.css":
+/*!********************************************************************************************************************************************!*\
+  !*** ./node_modules/css-loader/dist/cjs.js??ref--5-oneOf-2-1!./node_modules/postcss-loader/src??__nextjs_postcss!./pages/index.module.css ***!
+  \********************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/api.js */ "./node_modules/css-loader/dist/runtime/api.js")(true);
+// Module
+exports.push([module.i, "\n.index_small_container__2l9SZ {\n    opacity: 1;\n    margin: 0 auto;\n    max-width: 340px;\n}\n\n.index_hidden__1FSzE {\n    opacity: 0;\n}\n\n.index_title__27Lr- {\n    font-size: 2.7rem;\n    font-weight: 100;\n    margin: 10px 0;\n}\n\n.index_sub__17Oz9 {\n    font-weight: 100;\n    margin: 0;\n    margin: 10px 0;\n}\n\n.index_img_container__2aiD5 {\n    width: 100%;\n    margin: 35px auto 0;\n    text-align: center;\n}\n\n.index_profile__2s6Ln {\n    width: 100%;\n    background: #000;\n}\n\n.index_links__1Oz9g {\n    display: flex;\n    justify-content: space-between;\n}", "",{"version":3,"sources":["index.module.css"],"names":[],"mappings":";AACA;IACI,UAAU;IACV,cAAc;IACd,gBAAgB;AACpB;;AAEA;IACI,UAAU;AACd;;AAEA;IACI,iBAAiB;IACjB,gBAAgB;IAChB,cAAc;AAClB;;AAEA;IACI,gBAAgB;IAChB,SAAS;IACT,cAAc;AAClB;;AAEA;IACI,WAAW;IACX,mBAAmB;IACnB,kBAAkB;AACtB;;AAEA;IACI,WAAW;IACX,gBAAgB;AACpB;;AAEA;IACI,aAAa;IACb,8BAA8B;AAClC","file":"index.module.css","sourcesContent":["\n.small_container {\n    opacity: 1;\n    margin: 0 auto;\n    max-width: 340px;\n}\n\n.hidden {\n    opacity: 0;\n}\n\n.title {\n    font-size: 2.7rem;\n    font-weight: 100;\n    margin: 10px 0;\n}\n\n.sub {\n    font-weight: 100;\n    margin: 0;\n    margin: 10px 0;\n}\n\n.img_container {\n    width: 100%;\n    margin: 35px auto 0;\n    text-align: center;\n}\n\n.profile {\n    width: 100%;\n    background: #000;\n}\n\n.links {\n    display: flex;\n    justify-content: space-between;\n}"]}]);
+// Exports
+exports.locals = {
+	"small_container": "index_small_container__2l9SZ",
+	"hidden": "index_hidden__1FSzE",
+	"title": "index_title__27Lr-",
+	"sub": "index_sub__17Oz9",
+	"img_container": "index_img_container__2aiD5",
+	"profile": "index_profile__2s6Ln",
+	"links": "index_links__1Oz9g"
+};
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.828ac661448814bc678f.hot-update.js.map
